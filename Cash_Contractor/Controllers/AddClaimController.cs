c8bd586f7e6fec05b6b32e5e9ea9c85aa5f5af51
Cash_Contractor <dhanashreenarule93@gmail.com>
2020-06-02 23:53:28 +0530

    test

--- Cash_Contractor/Controllers/AddClaimController.cs
+++ Cash_Contractor/Controllers/AddClaimController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-Contractor 
+
 namespace Cash_Contractor.Controllers
 {
     public class AddClaimController : Controller
